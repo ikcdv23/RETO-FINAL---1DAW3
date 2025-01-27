dc05e14746d6c76476537e4f5271c35eacf26e99
Erik Tejada <ikdxb@plaiaundi.net>
2025-01-27 10:30:01 +0100

    Menu2

---
+++ src/funciones/FiltrarPrecio.java
@@ -0,0 +1,39 @@
+package funciones;
+import java.util.Scanner;
+import Clases.Pelicula;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import funciones.Menus;
+import conectorBD.conectorBD;
+
+public class FiltrarPrecio {
+	private  static Scanner scanner= new Scanner(System.in);
+	public static  void MostarPelisPorPrecio() throws SQLException {
+			 System.out.println("\n--- Mostrar peliculas por precio inferior al introducido ---");
+			 System.out.print("Ingresa el precio");
+
+
+			 String query = "SELECT * FROM peliculas WHERE precio < ?";
+			 try (PreparedStatement preparedStatement = conexion.prepareStatement(query)) {
+			     preparedStatement.setInt(1, precio);
+			     ResultSet resultSet = preparedStatement.executeQuery();
+
+			     if (!resultSet.isBeforeFirst()) {
+			         System.out.println("No se encontraron peliculas con precio inferior: " + precio);
+			     } else {
+			         while (resultSet.next()) {
+			        	 System.out.println("Nombre: " + resultSet.getString("nombre") +
+			                     ", Apellido1: " + resultSet.getInt("codigo") +
+			                     ", Apellido2: " + resultSet.getString("autor") +
+			                     ", FechaNacimiento: " + resultSet.getInt("precio") +
+			                     ", FechaNacimiento: " + resultSet.getString("genero"));
+			                     
+			         	}
+			         }
+			     }
+			 }
+
+	}
+	
+
